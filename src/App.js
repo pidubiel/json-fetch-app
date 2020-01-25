7a1ffa972b2ede7bc9ccc9e3e377d301151f4064
Piotr Dubiel <piotrdubiel@icloud.com>
2020-01-25 10:23:05 +0100

    Created ArticlesList component with first 10 posts from jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './styles/index.scss';
 import { ArticlesList, AddArticle, Home } from './pages';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [articles, setArticles] = useState([]);
+  const [error, setError] = useState(false);
+
+  const fetchArticles = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => setArticles(res.data.filter(el => el.id <= 10)))
+      .catch(err => setError(err));
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={Home} />
-        <Route path='/articles-list' component={ArticlesList} />
+        <Route path='/articles-list'>
+          <ArticlesList articles={articles} error={error} />
+        </Route>
         <Route path='/add-article' component={AddArticle} />
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- src/pages/ArticlesList/index.js
+++ src/pages/ArticlesList/index.js
@@ -1,50 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-class ArticlesList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoaded: false,
-      articles: []
-    };
-  }
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(res => res.json())
-      .then(
-        result => {
-          this.setState({
-            isLoaded: true,
-            articles: result
-          });
-        },
-        error => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      );
-  }
-  render() {
-    console.log(this.state.articles);
-    return (
-      <div className='main-container'>
-        <Link className='link' to='/'>
-          Back to home
-        </Link>
-        <div className='articles'>
-          {this.state.articles.map(el => (
+const ArticlesList = ({ articles, error }) => {
+  return (
+    <div className='main-container'>
+      <Link className='link' to='/'>
+        Back to home
+      </Link>
+      <div className='articles'>
+        {error && <p>{error.message}</p>}
+        {!error &&
+          articles &&
+          articles.length > 0 &&
+          articles.map(el => (
             <div className='articles__item' key={el.id}>
               <h1>{el.title}</h1>
               <p>{el.body}</p>
             </div>
           ))}
-        </div>
       </div>
-    );
-  }
-}
-
+    </div>
+  );
+};
 export default ArticlesList;
